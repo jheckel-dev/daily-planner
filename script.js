d61f71d33105c887bdf79f33c6ffe7a93acf9736
jheckel <jalen.heckel@icloud.com>
2021-07-19 10:38:16 -0700

    remaining time boxes for 11,12,1,2,3,4,5 function in moment.

--- script.js
+++ script.js
@@ -23,3 +23,31 @@ $('.block1').text(time1)
 var time2 = beforeTime.add(1, 'h');
 time2 = time2.format('hh:mm A');
 $('.block2').text(time2);
+// 11am
+var time3 = beforeTime.add(1, 'h');
+time3 = time3.format('hh:mm A');
+$('.block3').text(time3);
+// 12pm
+var time4 = beforeTime.add(1, 'h');
+time4 = time4.format('hh:mm A');
+$('.block4').text(time4);
+// 1pm
+var time5 = beforeTime.add(1, 'h');
+time5 = time5.format('hh:mm A');
+$('.block5').text(time5);
+// 2pm
+var time6 = beforeTime.add(1, 'h');
+time6 = time6.format('hh:mm A');
+$('.block6').text(time6);
+// 3pm
+var time7 = beforeTime.add(1, 'h');
+time7 = time7.format('hh:mm A');
+$('.block7').text(time7);
+// 4pm
+var time8 = beforeTime.add(1, 'h');
+time8 = time8.format('hh:mm A');
+$('.block8').text(time8);
+// 5pm
+var time9 = beforeTime.add(1, 'h');
+time9 = time9.format('hh:mm A');
+$('.block9').text(time9);
