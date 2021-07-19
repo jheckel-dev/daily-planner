5aba8ce2549f78302e83c8446b8f5e6924bd5670
jheckel <jalen.heckel@icloud.com>
2021-07-19 10:31:17 -0700

    added 10am time box

--- script.js
+++ script.js
@@ -19,3 +19,7 @@ var beforeTime = moment().startOf('day').add(9, 'hours');
 var time1 = beforeTime.add(0, 'h');
 time1 = time1.format('hh:mm A');
 $('.block1').text(time1)
+// 10am
+var time2 = beforeTime.add(1, 'h');
+time2 = time2.format('hh:mm A');
+$('.block2').text(time2);
