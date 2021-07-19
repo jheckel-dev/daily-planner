d0717b403cf003257da13f9b721f3be0e754ddab
jheckel <jalen.heckel@icloud.com>
2021-07-19 10:29:05 -0700

    added functioning time box for 9am.

--- script.js
+++ script.js
@@ -5,3 +5,17 @@ $('.saveBtn').click(function(){
     var listItem = $(this).parent().data('hour');
     localStorage.setItem(listItem, formValue);
 });
+// declaring day and time for moment
+moment(Date);
+$('#currentDay').text(moment().format('dddd MMMM Do YYYY, h:mm a'));
+// grabs current time from moment
+var currentTime = moment();
+// converts current time to nearest hour
+currentTime = currentTime.startOf('hour');
+// adds 9hrs to start of day to set start time of calendar to 9am
+var beforeTime = moment().startOf('day').add(9, 'hours');
+// setting time blocks
+// 9am
+var time1 = beforeTime.add(0, 'h');
+time1 = time1.format('hh:mm A');
+$('.block1').text(time1)
